9e0073cf10ffc333497df35eee76665009651649
Mila Wilkinson <miladijah@gmail.com>
2015-12-07 18:02:45 -0800

    updating logout3

--- server/controllers/usersSC.js
+++ server/controllers/usersSC.js
@@ -95,7 +95,7 @@ module.exports = (function(){
          		    //Session destroyed, redirect to home page.
          		    res.redirect('/');
          		}
-         	}
+         	})
 		},
 
 		authenticate: function(req, res){
